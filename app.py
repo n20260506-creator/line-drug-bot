bef44e367f3f77e35741503eb1cb99149337a6d1
n20260506-creator <n20260506@gmail.com>
2026-09-14 16:10:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -225,9 +225,10 @@ def handle_image_message(event):
             )
             
             # 4. 呼叫 Gemini (2.0-flash)
-            candidate_models = [
-                'gemini-2.0-flash',
-               ]
+             candidate_models = [
+            'gemini-2.0-flash',
+            'gemini-2.0-flash-lite' # 增加備用模型，提高穩定度
+                ]
             response = None
             last_error = None
 
